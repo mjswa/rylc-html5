78bbf601fb56c3e3eae9c7fdac53ca0db59acf9e
Tobias Bosch <tobias.bosch@opitz-consulting.com>
2012-09-05 10:54:12 +0200

    update to jqm 1.1.1 and new jqm-angular-adapter

--- src/main/webapp/lib/jqmExternalAsEmbeddedPages.js
+++ src/main/webapp/lib/jqmExternalAsEmbeddedPages.js
@@ -3,8 +3,20 @@
  * as external pages with the suffix .html.
  */
 define(["jquery", "jquery.mobile"], function($) {
+  var initialPage;
+  var initialPageId = $.mobile.path.stripHash(location.hash);
+  if (initialPageId && !$('#'+initialPageId).length) {
+    $(document).bind("pagecontainercreate", function() {
+        initialPage = $('<div data-role="page" id="'+initialPageId+'"></div>');
+        $.mobile.pageContainer.append(initialPage);
+    });
+  }
+
   $.mobile.originalLoadPage = $.mobile.loadPage;
   $.mobile.loadPage = function (url, options) {
+    if (initialPage) {
+        initialPage.remove();
+    }
     var match = url.match(/#(\w+)/);
     if (match) {
       var pageId = match[1];
